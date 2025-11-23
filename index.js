050babf210e9c615515e65661b27e5722c42f5b4
viatnum21 <shahzadahmadahmad2111@gmail.com>
2025-11-23 10:40:46 -0800

    Final changes for verification

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const path = require('path'); // Yeh line zaroori hai
 const app = express();
 const PORT = process.env.PORT || 8081;
+
+// YEH LINE AAPKE FRONT-END AUR VERIFICATION FILE KO SERVE KAREGI
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.send('Hello! My server is live.');
